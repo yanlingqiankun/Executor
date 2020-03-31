e452cac80081fa53b8f291fa7c03b0e5ea60dba6
yanlingqiankun <1832081182@qq.com>
2020-03-31 17:52:10 +0800

    增加了获得随机id的模块

---
+++ stringid/stringid.go
@@ -0,0 +1,72 @@
+package stringid
+
+import (
+	cryptorand "crypto/rand"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"math/rand"
+	"regexp"
+	"time"
+)
+
+var (
+	validHex = regexp.MustCompile(`^[a-f0-9]{64}$`)
+)
+
+func init() {
+	// 初始化全局随机数种子
+	var seed int64
+	buffer := make([]byte, 8)
+	if _, err := cryptorand.Read(buffer); err != nil {
+		seed = time.Now().UnixNano()
+	} else {
+		seed = int64(binary.BigEndian.Uint64(buffer))
+	}
+
+	rand.Seed(seed)
+}
+
+// 生成256bit随机ID
+func GenerateRandomID() string {
+	buffer := make([]byte, 32)
+	if _, err := cryptorand.Read(buffer); err != nil {
+		rand.Read(buffer)
+	}
+	for {
+		id, success := toHexID(buffer)
+		if success {
+			return id
+		}
+	}
+}
+
+// 生成256bit伪随机ID
+func GenerateNonCryptoID() string {
+	buffer := make([]byte, 32)
+	rand.Read(buffer)
+	for {
+		id, success := toHexID(buffer)
+		if success {
+			return id
+		}
+	}
+}
+
+func toHexID(buffer []byte) (string, bool) {
+	id := hex.EncodeToString(buffer)
+	// 如果要将id作为hostname，必须判断是否全为数字，某些语言处理全数字hostname时会出错
+	// https://github.com/moby/moby/issues/3869
+	//if _, err := strconv.ParseInt(TruncateID(id), 10, 64); err == nil {
+	//	return "", false
+	//}
+	return id, true
+}
+
+// 验证ID是否有效
+func ValidateID(id string) error {
+	if ok := validHex.MatchString(id); !ok {
+		return fmt.Errorf("ID %q is invalid", id)
+	}
+	return nil
+}
